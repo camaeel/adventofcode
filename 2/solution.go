c8dd68b66da8cdcd24d08f9a55183a1f5e699558
Kamil <12999736+camaeel@users.noreply.github.com>
2019-12-04 14:19:18 +0100

    solution 2

--- 2/solution.go
+++ 2/solution.go
@@ -14,11 +14,34 @@ func main() {
 	program := readProgram()
 	fmt.Println("Input program: ", program)
 	//debug settings
-	program[1] = 12
-	program[2] = 2
 
-	result := intcode.Intcode(program)
-	fmt.Println("Resulting program: ", result)
+	tmpProg := make([]int, len(program))
+	copy(tmpProg, program)
+	tmpProg[1] = 12
+	tmpProg[2] = 2
+
+	result := intcode.Intcode(tmpProg)
+	fmt.Println("Input program: ", program)
+	fmt.Println("Resulting program part 1: ", result)
+
+	for noun := 0; noun < 100; noun++ {
+		for verb := 0; verb < 100; verb++ {
+			if GetResult(program, noun, verb) == 19690720 {
+				fmt.Println("Got: ", 19690720, ", verb= ", verb, ", noun=", noun)
+				fmt.Println("Rsult : ", noun*100+verb)
+			}
+		}
+	}
+}
+
+func GetResult(program []int, noun int, verb int) int {
+	tmpProg := make([]int, len(program))
+	copy(tmpProg, program)
+	tmpProg[1] = noun
+	tmpProg[2] = verb
+	intcode.Intcode(tmpProg)
+
+	return tmpProg[0]
 }
 
 func readProgram() []int {
