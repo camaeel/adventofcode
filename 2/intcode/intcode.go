3e073dc6a9c65e6eb4d94dcb2e82db7767569c1c
Kamil <12999736+camaeel@users.noreply.github.com>
2019-12-04 13:27:03 +0100

    working computa

--- 2/intcode/intcode.go
+++ 2/intcode/intcode.go
@@ -1,9 +1,24 @@
 package intcode
 
+import "strconv"
+
 func Intcode(program []int) []int {
 
 	for counter := 0; program[counter] != 99; {
-		counter++
+		if program[counter] != 1 && program[counter] != 2 {
+			panic("Illegal operand: " + strconv.Itoa(program[counter]) + ", at counter: " + strconv.Itoa(counter))
+		}
+		srcIndex1 := program[counter+1]
+		srcIndex2 := program[counter+2]
+		tgtIndex := program[counter+3]
+
+		if program[counter] == 1 {
+			program[tgtIndex] = program[srcIndex1] + program[srcIndex2]
+		} else if program[counter] == 2 {
+			program[tgtIndex] = program[srcIndex1] * program[srcIndex2]
+		}
+
+		counter += 4
 	}
 	return program
 
