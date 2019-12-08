b05247e8656d6888b54b2ab42be83500c1197241
Kamil <12999736+camaeel@users.noreply.github.com>
2019-12-08 23:56:54 +0100

    day 8 part 2

--- 8/solution.go
+++ 8/solution.go
@@ -56,6 +56,38 @@ func main() {
 
 	fmt.Println("Part 1: Min zeros layer:", minZerosLayer, ", code:", ones[minZerosLayer]*twos[minZerosLayer])
 
+	result := make([]int, width*height)
+
+	for i := 0; i < (width * height); i++ {
+		pixel := 2
+		// fmt.Println()
+		// fmt.Println("Pixel: ", i)
+		// fmt.Println()
+		for layer := 0; layer < len(layers) && pixel == 2; layer++ {
+			pixel = layers[layer][i]
+			// fmt.Print(pixel)
+
+		}
+		result[i] = pixel
+	}
+
+	for h := 0; h < height; h++ {
+		for w := 0; w < width; w++ {
+
+			switch result[w+h*width] {
+			case 0:
+				fmt.Print(" ")
+			case 1:
+				fmt.Print("\u2B1B")
+			case 2:
+				fmt.Print("X")
+			default:
+				panic("unknown digit")
+			}
+		}
+		fmt.Println()
+	}
+
 }
 
 func check(e error) {
