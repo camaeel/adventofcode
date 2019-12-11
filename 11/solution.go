9f50bf2201d8915d13499c6bc26f2861bd208c76
Kamil <12999736+camaeel@users.noreply.github.com>
2019-12-11 21:07:26 +0100

    11 - part 1

--- 11/solution.go
+++ 11/solution.go
@@ -34,39 +34,38 @@ func main() {
 
 	for finished == false {
 		computer.Input <- GetColor(painted, position)
-		dirCmd, result := <-computer.Output
-		color, result := <-computer.Output
+		color, result1 := <-computer.Output
+		dirCmd, result2 := <-computer.Output
 
-		position, direction = Move(position, direction, dirCmd)
-		position.Color = color
+		if result1 == true && result2 == true {
 
-		found := false
-		for _, v := range painted {
-			if v.X == position.X && v.Y == position.Y {
-				found = true
+			position.Color = color
+
+			found := false
+			for _, v := range painted {
+				if v.X == position.X && v.Y == position.Y {
+					v.Color = color
+					found = true
+					break
+				}
 			}
-		}
-		if !found {
-			painted = append(painted, position)
-		}
-
-		if result == false {
+			if !found {
+				painted = append(painted, &Point{X: position.X, Y: position.Y, Color: color})
+			}
+			position, direction = Move(position, direction, dirCmd)
+		} else {
 			finished = true
 		}
 	}
 
-	fmt.Println("result:", computer.OutputArray)
-	fmt.Println("len result:", len(computer.OutputArray))
-}
-
-func Paint(painted []*Point, position Point) {
-
+	// fmt.Println("result:", computer.OutputArray)
+	fmt.Println("len result:", len(painted))
 }
 
 func GetColor(painted []*Point, position Point) int {
 	for _, p := range painted {
 		if p.X == position.X && p.Y == position.Y {
-			return 1
+			return p.Color
 		}
 	}
 	return 0
@@ -75,8 +74,10 @@ func GetColor(painted []*Point, position Point) int {
 func Move(position Point, direction Direction, dirCmd int) (Point, Direction) {
 	if dirCmd == 1 {
 		direction = (direction + 1) % 4
-	} else {
+	} else if dirCmd == 0 {
 		direction = (direction + 3) % 4
+	} else {
+		panic("Unknown direction")
 	}
 	if direction == Up {
 		position.Y++
