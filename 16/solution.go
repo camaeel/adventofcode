f6165d6a1ed875d281b2decf0e17d62f85378633
Kamil <12999736+camaeel@users.noreply.github.com>
2019-12-16 23:44:58 +0100

    uproszczenie

--- 16/solution.go
+++ 16/solution.go
@@ -33,7 +33,8 @@ func FFTPhase(arr []int, pattern []int) []int {
 	res := make([]int, 0)
 	for i := 0; i < len(arr); i++ {
 		value := 0
-		for j, v := range arr {
+		for j := i; j < len(arr); j++ {
+			v := arr[j]
 			mult := pattern[((j+1)/(i+1))%len(pattern)]
 			// fmt.Print(v, " * ", mult, " + ")
 			value += v * mult
@@ -45,8 +46,25 @@ func FFTPhase(arr []int, pattern []int) []int {
 	return res
 }
 
+func FFTPhase2(arr []int, pattern []int, offset int, length int) []int {
+	res := make([]int, len(arr))
+	for i := offset; i < len(arr) && i < offset+length; i++ {
+		value := 0
+		for j, v := range arr {
+			mult := pattern[((j+1)/(i+1))%len(pattern)]
+			// fmt.Print(v, " * ", mult, " + ")
+			value += v * mult
+		}
+		z := int(math.Abs(float64(value))) % 10
+		// fmt.Println("=", z)
+		res[i] = z
+	}
+	return res
+}
+
 func FFT(arr []int, pattern []int, iterations int) []int {
 	for i := 0; i < iterations; i++ {
+		fmt.Println("1: Iteration:", i)
 		arr = FFTPhase(arr, pattern)
 	}
 	return arr
@@ -54,4 +72,12 @@ func FFT(arr []int, pattern []int, iterations int) []int {
 
 func Input() string {
 	return "59713137269801099632654181286233935219811755500455380934770765569131734596763695509279561685788856471420060118738307712184666979727705799202164390635688439701763288535574113283975613430058332890215685102656193056939765590473237031584326028162831872694742473094498692690926378560215065112055277042957192884484736885085776095601258138827407479864966595805684283736114104361200511149403415264005242802552220930514486188661282691447267079869746222193563352374541269431531666903127492467446100184447658357579189070698707540721959527692466414290626633017164810627099243281653139996025661993610763947987942741831185002756364249992028050315704531567916821944"
+}
+
+func FFT2(arr []int, pattern []int, iterations int, offset int, length int) []int {
+	for i := 0; i < iterations; i++ {
+		fmt.Println("2: Iteration:", i)
+		arr = FFTPhase2(arr, pattern, offset, length)
+	}
+	return arr
 }
--- 16/solution_test.go
+++ 16/solution_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
 )
 
@@ -20,8 +22,38 @@ func TestPart1(t *testing.T) {
 	pattern := []int{0, 1, 0, -1}
 	expected := "10189359"
 	result := ArrToString(FFT(StringToArr(input), pattern, 100))
-
+	fmt.Println("Resul:", result)
 	if result[0:8] != expected {
+		t.Error("Error. Expected:", expected, ", Got:", result)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	input := ""
+	for i := 0; i < 10000; i++ {
+		input += Input()
+	}
+	pattern := []int{0, 1, 0, -1}
+	expected := "10189359"
+	offset, _ := strconv.Atoi(Input()[0:7])
+	result := ArrToString(FFT2(StringToArr(input), pattern, 100, offset, 8))
+
+	if result[offset:offset+8] != expected {
+		t.Error("Error. Expected:", expected, ", Got:", result)
+	}
+}
+
+func TestPart2Long(t *testing.T) {
+	input := ""
+	for i := 0; i < 10000; i++ {
+		input += Input()
+	}
+	pattern := []int{0, 1, 0, -1}
+	expected := "10189359"
+	offset, _ := strconv.Atoi(Input()[0:7])
+	result := ArrToString(FFT(StringToArr(input), pattern, 100))
+
+	if result[offset:offset+8] != expected {
 		t.Error("Error. Expected:", expected, ", Got:", result)
 	}
 }
